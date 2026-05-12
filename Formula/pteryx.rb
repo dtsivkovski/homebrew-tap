d591c2b68705b3759b0af64d972cb7b9e82ae3b0
Daniel Tsivkovski <89223402+dtsivkovski@users.noreply.github.com>
2026-05-12 10:34:48 -0700

    Update pteryx to 0.1.4

--- Formula/pteryx.rb
+++ Formula/pteryx.rb
@@ -1,8 +1,8 @@
 class Pteryx < Formula
   desc "Forensic file inspection tool for signatures and hash baselines"
   homepage "https://github.com/dtsivkovski/pteryx"
-  url "https://github.com/dtsivkovski/pteryx/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "bc36bbfd6013855424a0929794eab1b7777ac1b98baf7bed868adfce600ebdcc"
+  url "https://github.com/dtsivkovski/pteryx/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "4807d45b78fcd214324bc4731f6051915c885f175f44a7b9d4117fd7412c7b4e"
   license "MIT"
 
   depends_on "go" => :build
